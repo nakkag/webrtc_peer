a1e791dde4d48f3d7ed432f1c10c0c8777be395e
nakashima.tomoaki <nakashima.tomoaki@nalgo.co.jp>
2022-10-25 11:54:58 +0900

    first commit

---
+++ webrtc.js
@@ -0,0 +1,256 @@
+let serverConnection;
+let localVideo, remoteVideo;
+let localPeerConnection = null, remotePeerConnection = null;
+let localQueue = new Array(), remoteQueue = new Array();
+let localId, remoteId;
+let localSid = 0, remoteSid = 0;
+
+const sslPort = 8443;
+const peerConnectionConfig = {
+	iceServers: [
+		// GoogleのパブリックSTUNサーバーを指定しているが自前のSTUNサーバーがあれば変更する
+		{urls: 'stun:stun.l.google.com:19302'},
+		{urls: 'stun:stun1.l.google.com:19302'},
+		{urls: 'stun:stun2.l.google.com:19302'},
+		// TURNサーバーがあれば指定する
+		//{urls: 'turn:turn_server', username:'', credential:''}
+	]
+};
+
+document.onreadystatechange = function() {
+	// Local IDとRemote IDを入力する
+	// Local IDとRemote IDは別々の値を入力する
+	// Remote IDと対向のLocal IDが一致するとカメラ接続が開始する
+	while (!localId) {
+		localId = window.prompt('Local ID', '');
+	}
+	while (!remoteId) {
+		remoteId = window.prompt('Remote ID', '');
+	}
+	startWebRTC(localId, remoteId);
+}
+
+function startWebRTC(localId, remoteId) {
+	localVideo = document.getElementById('localVideo');
+	if (localVideo) {
+		localVideo.srcObject = null;
+	}
+	remoteVideo = document.getElementById('remoteVideo');
+	stopVideo();
+
+	if (serverConnection) {
+		serverConnection.close();
+	}
+	// サーバー接続の開始
+	serverConnection = new WebSocket('wss://' + location.hostname + ':' + sslPort + '/');
+	serverConnection.onmessage = gotMessageFromServer;
+	serverConnection.onopen = function(event) {
+		// サーバーに接続情報を通知
+		this.send(JSON.stringify({open: {local: localId, remote: remoteId}}));
+	};
+	serverConnection.onclose = function(event) {
+		clearInterval(this.timer);
+		setTimeout(function(conn) {
+			if (serverConnection === conn) {
+				// 一定時間経過後にサーバーへ再接続
+				startWebRTC(localId, remoteId);
+			}
+		}, 5000, this);
+	}
+	serverConnection.timer = setInterval(function() {
+		// 接続確認
+		serverConnection.send(JSON.stringify({ping: 1}));
+	}, 30000);
+}
+
+function startVideo() {
+	if (navigator.mediaDevices.getUserMedia) {
+		if (window.stream) {
+			// 既存のストリームを破棄
+			try {
+				window.stream.getTracks().forEach(track => {
+					track.stop();
+				});
+			} catch(error) {
+				console.error(error);
+			}
+			window.stream = null;
+		}
+		// カメラとマイクの開始
+		const constraints = {
+			audio: true,
+			video: true
+		};
+		navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+			window.stream = stream;
+			localVideo.srcObject = stream;
+			localStart();
+		}).catch(e => {
+			console.error(error);
+			alert('WebCam Start Error.\n\n' + error);
+		});
+	} else {
+		alert('Your browser does not support getUserMedia API');
+	}
+}
+
+function stopVideo() {
+	if (remoteVideo && remoteVideo.srcObject) {
+		try {
+			remoteVideo.srcObject.getTracks().forEach(track => {
+				track.stop();
+			});
+		} catch(error) {
+			console.error(error);
+		}
+		remoteVideo.srcObject = null;
+	}
+	if (localPeerConnection !== null) {
+		localPeerConnection.close();
+		localPeerConnection = null;
+	}
+	if (remotePeerConnection !== null) {
+		remotePeerConnection.close();
+		remotePeerConnection = null;
+	}
+	localQueue = new Array();
+	remoteQueue = new Array();
+}
+
+function localStart() {
+	// Local接続IDの生成、複数のICEメッセージが混在する場合に今のセッションを判断するのに利用する
+	localSid = new Date().getTime().toString(16) + Math.floor(Math.random() * 1000).toString(16);
+	if (localPeerConnection !== null) {
+		localPeerConnection.close();
+	}
+	// Local接続の開始
+	localPeerConnection = new RTCPeerConnection(peerConnectionConfig);
+	localPeerConnection.onicecandidate = gotIceCandidateLocal;
+	if (window.stream) {
+		window.stream.getTracks().forEach(track => localPeerConnection.addTrack(track, window.stream));
+	}
+	localPeerConnection.createOffer().then(createdDescription).catch(errorHandler);
+}
+
+function remoteStart() {
+	if (remotePeerConnection !== null) {
+		remotePeerConnection.close();
+	}
+	// Remote接続の開始
+	remotePeerConnection = new RTCPeerConnection(peerConnectionConfig);
+	remotePeerConnection.onicecandidate = gotIceCandidateRemote;
+	remotePeerConnection.ontrack = gotRemoteStream;
+}
+
+function gotMessageFromServer(message) {
+	const signal = JSON.parse(message.data);
+	if (signal.start) {
+		// サーバーからの「start」を受けてビデオを開始する
+		startVideo();
+		return;
+	}
+	if (signal.close) {
+		// 接続先の終了通知
+		stopVideo();
+		return;
+	}
+	if (signal.ping) {
+		serverConnection.send(JSON.stringify({pong: 1}));
+		return;
+	}
+	if (signal.re_offer) {
+		// 再オファーの要求
+		if (signal.sid !== undefined && signal.sid !== remoteSid) {
+			return;
+		}
+		// Local接続を再スタートする
+		localStart();
+		return;
+	}
+	if (signal.sdp) {
+		if (signal.sdp.type === 'offer') {
+			if (signal.sid !== undefined) {
+				// Remote接続IDを保存
+				remoteSid = signal.sid;
+			}
+			// Remote接続の開始
+			remoteStart();
+			remotePeerConnection.setRemoteDescription(signal.sdp).then(function() {
+				remotePeerConnection.createAnswer().then(gotAnswer).catch(errorHandler);
+			}).catch(errorHandler);
+		} else if (signal.sdp.type === 'answer') {
+			if (!localPeerConnection || (signal.sid !== undefined && signal.sid !== localSid)) {
+				return;
+			}
+			localPeerConnection.setRemoteDescription(signal.sdp).catch(errorHandler);
+			if (!remotePeerConnection || !remotePeerConnection.remoteDescription) {
+				// Remote接続が開始していないので再オファーを要求
+				serverConnection.send(JSON.stringify({'re_offer': 1, 'remote': remoteId, 'sid': localSid}));
+			}
+		}
+	} else if (signal.ice_r) {
+		if (remotePeerConnection && remotePeerConnection.remoteDescription) {
+			if (signal.sid === undefined || signal.sid === remoteSid) {
+				remotePeerConnection.addIceCandidate(new RTCIceCandidate(signal.ice_r)).catch(errorHandler);
+			}
+		} else {
+			// Remote接続が開始していないのでRemoteキューに貯める
+			remoteQueue.push(message);
+			return;
+		}
+	} else if (signal.ice_l) {
+		if (localPeerConnection && localPeerConnection.remoteDescription) {
+			if (signal.sid === undefined || signal.sid === localSid) {
+				localPeerConnection.addIceCandidate(new RTCIceCandidate(signal.ice_l)).catch(errorHandler);
+			}
+		} else {
+			// Local接続が開始していないのでLocalキューに貯める
+			localQueue.push(message);
+			return;
+		}
+	}
+	if (remoteQueue.length > 0 && remotePeerConnection && remotePeerConnection.remoteDescription) {
+		// Remoteキューのメッセージを再処理
+		gotMessageFromServer(remoteQueue.shift());
+	}
+	if (localQueue.length > 0 && localPeerConnection && localPeerConnection.remoteDescription) {
+		// Localキューのメッセージを再処理
+		gotMessageFromServer(localQueue.shift());
+	}
+}
+
+function gotIceCandidateLocal(event) {
+	if (event.candidate != null) {
+		serverConnection.send(JSON.stringify({ice_r: event.candidate, remote: remoteId, sid: localSid}));
+	}
+}
+
+function gotIceCandidateRemote(event) {
+	if (event.candidate != null) {
+		serverConnection.send(JSON.stringify({ice_l: event.candidate, remote: remoteId, sid: remoteSid}));
+	}
+}
+
+function createdDescription(description) {
+	localPeerConnection.setLocalDescription(description).then(function() {
+		serverConnection.send(JSON.stringify({sdp: localPeerConnection.localDescription, remote: remoteId, sid: localSid}));
+	}).catch(errorHandler);
+}
+
+function gotAnswer(description) {
+	remotePeerConnection.setLocalDescription(description).then(function() {
+		serverConnection.send(JSON.stringify({sdp: remotePeerConnection.localDescription, remote: remoteId, sid: remoteSid}));
+	}).catch(errorHandler);
+}
+
+function gotRemoteStream(event) {
+	if (event.streams && event.streams[0]) {
+		remoteVideo.srcObject = event.streams[0];
+	} else {
+		remoteVideo.srcObject = new MediaStream(event.track);
+	}
+}
+
+function errorHandler(error) {
+	alert('WebCam Start Error.\n\n' + error);
+}
---
+++ webrtc_server.js
@@ -0,0 +1,123 @@
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+
+const sslPort = 8443;
+const serverConfig = {
+    // SSL証明書、環境に合わせてパスを変更する
+	key: fs.readFileSync('privkey.pem'),
+	cert: fs.readFileSync('fullchain.pem')
+};
+
+// 接続リスト
+let connections = [];
+
+// WebSocket処理
+const socketProc = function(ws, req) {
+	ws.pingTimer = setInterval(function() {
+		if (ws.readyState === WebSocket.OPEN) {
+			// 接続確認
+			ws.send(JSON.stringify({ping: 1}));
+		}
+	}, 180000);
+
+	ws.on('message', function(message) {
+		const json = JSON.parse(message);
+		if (json.open) {
+			console.log('open: ' + ws._socket.remoteAddress + ': local=' + json.open.local + ', remote=' + json.open.remote);
+			if (!json.open.local || !json.open.remote) {
+				return;
+			}
+			// 同一IDが存在するときは古い方を削除
+			connections = connections.filter(function (data, i) {
+				if (data.local === json.open.local && data.remote === json.open.remote) {
+					return false;
+				}
+				return true;
+			});
+			// 接続情報を保存
+			connections.push({local: json.open.local, remote: json.open.remote, ws: ws});
+			ws.send(JSON.stringify({start: 1}));
+			return;
+		}
+		if (json.error) {
+			console.error('client_error: ' + ws._socket.remoteAddress + ': ' + json.error);
+			return;
+		}
+		if (json.pong) {
+			return;
+		}
+		if (json.ping) {
+			if (ws.readyState === WebSocket.OPEN) {
+				ws.send(JSON.stringify({pong: 1}));
+			}
+			return;
+		}
+		// 対向の接続を検索
+		connections.some(function(data) {
+			if (data.local === json.remote && data.ws.readyState === WebSocket.OPEN) {
+				// WebRTC情報の転送
+				data.ws.send(JSON.stringify(json));
+				return true;
+			}
+		});
+	});
+
+	ws.on('close', function () {
+		closeConnection(ws);
+		console.log('close: ' + ws._socket.remoteAddress);
+	});
+
+	ws.on('error', function(error) {
+		closeConnection(ws);
+		console.error('error: ' + ws._socket.remoteAddress + ': ' + error);
+	});
+
+	function closeConnection(conn) {
+		connections = connections.filter(function (data, i) {
+			if (data.ws !== conn) {
+				return true;
+			}
+			connections.some(function(remoteData) {
+				if (remoteData.local === data.remote && remoteData.ws.readyState === WebSocket.OPEN) {
+					// 対向に切断を通知
+					remoteData.ws.send(JSON.stringify({close: 1}));
+					return true;
+				}
+			});
+			data.ws = null;
+			return false;
+		});
+		if (conn.pingTimer) {
+			clearInterval(conn.pingTimer);
+			conn.pingTimer = null;
+		}
+	}
+};
+
+// 静的ファイル処理
+const service = function(req, res) {
+	const file = path.join(process.cwd(), req.url);
+	fs.stat(file, (err, stat) => {
+		if (err) {
+			res.writeHead(404);
+			res.end();
+			return;
+		}
+		if (stat.isDirectory()) {
+			service({url: `${req.url}/index.html`}, res);
+		} else if (stat.isFile()) {
+			const stream = fs.createReadStream(file);
+			stream.pipe(res);
+		}
+	});
+};
+
+// HTTPSサーバの開始
+const httpsCamServer = https.createServer(serverConfig, service);
+httpsCamServer.listen(sslPort, '0.0.0.0');
+// WebSocketの開始
+const wss_cam = new WebSocket.Server({server: httpsCamServer});
+wss_cam.on('connection', socketProc);
+console.log('Server running.');
